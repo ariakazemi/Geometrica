7d6dafbd452f70efee04a0f92f59df63bbebecbb
Aria <ark.d.night@gmail.com>
2016-04-28 21:23:33 +0430

    Unity Files Added

---
+++ Development/Geometrica/Assets/Standard Assets/Scripts/Camera Scripts/CameraPixelPerfect.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPixelPerfect : MonoBehaviour {
+
+	public static float pixelRatio = 1.0f;
+	public static float scale = 1.0f;
+
+	public Vector2 RES = new Vector2(700	, 300);
+
+	void Awake () {
+		var camera = GetComponent<Camera> ();
+
+		if (camera.orthographic) {
+			Debug.Log(Screen.height);
+			Debug.Log(RES.y);
+			scale = Screen.height / RES.y;
+			Debug.Log(scale);
+			pixelRatio *= scale;
+			Debug.Log(pixelRatio);
+
+			camera.orthographicSize = (Screen.height / 2.0f) / pixelRatio;
+			Debug.Log(Screen.height / 2.0f);
+		}
+	}
+
+}
---
+++ Development/Geometrica/Assets/Standard Assets/Scripts/General Scripts/move.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+	private Rigidbody2D body2d;
+
+	public static float speed = 1.0f;
+	// Use this for initialization
+	void Awake () {
+		body2d = GetComponent<Rigidbody2D> ();
+	}
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 v = new Vector2(0,0);
+		if (Input.GetKey (KeyCode.W)) {
+			v.y = speed;
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			v.y = -speed;
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			v.x = -speed;
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			v.x = speed;
+		}
+		body2d.velocity = v;
+
+	}
+}
