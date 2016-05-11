4cbc6a32fbe08722de9a9333851e434cc4f0fb32
sepehr <sepehr2012a@gmail.com>
2016-05-11 15:20:40 +0430

    bowl shit
    more bowlshit

--- Development/Geometrica/Assets/Standard Assets/Scripts/General Scripts/move.cs
+++ Development/Geometrica/Assets/Standard Assets/Scripts/General Scripts/move.cs
@@ -3,10 +3,13 @@ using System.Collections;
 
 public class move : MonoBehaviour {
 	private Rigidbody2D body2d;
-
+	bool left,rigth,up,down;
 	public static float speed = 1.0f;
+	public static float maxSpeed = 3.0f;
+	public static float Acc=0.1f;
 	// Use this for initialization
 	void Awake () {
+		left = rigth = up = down = false;
 		body2d = GetComponent<Rigidbody2D> ();
 	}
 
@@ -17,16 +20,52 @@ public class move : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Vector2 v = new Vector2(0,0);
+
+
+
 		if (Input.GetKey (KeyCode.W)) {
+			if (!up) {
+				up = true;
+				down = rigth = left = false;
+				speed = 1.0f;
+			} else {
+				if (speed < maxSpeed)
+					speed += Acc;
+
+			}
 			v.y = speed;
 		}
 		if (Input.GetKey (KeyCode.S)) {
+			if (!down) {
+				down = true;
+				up = rigth = left = false;
+				speed = 1.0f;
+			} else {
+				if (speed < maxSpeed)
+					speed += Acc;
+			}
 			v.y = -speed;
 		}
 		if (Input.GetKey (KeyCode.A)) {
+			if (!left) {
+				left = true;
+				up = rigth = down = false;
+				speed = 1.0f;
+			} else {
+				if (speed < maxSpeed)
+					speed += Acc;
+			}
 			v.x = -speed;
 		}
 		if (Input.GetKey (KeyCode.D)) {
+			if (!rigth) {
+				rigth = true;
+				up = left = down = false;
+				speed = 1.0f;
+			} else {
+				if (speed < maxSpeed)
+					speed += Acc;
+			}
 			v.x = speed;
 		}
 		body2d.velocity = v;
