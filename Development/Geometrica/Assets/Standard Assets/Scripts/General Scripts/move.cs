9303f7862df3ecbab9b2fefc2334b59cdc56f513
sepehr <sepehr2012a@gmail.com>
2016-05-17 10:18:15 +0430

    move changes
    circle now have sort of friction and can ACC in multiply directions

--- Development/Geometrica/Assets/Standard Assets/Scripts/General Scripts/move.cs
+++ Development/Geometrica/Assets/Standard Assets/Scripts/General Scripts/move.cs
@@ -4,9 +4,10 @@ using System.Collections;
 public class move : MonoBehaviour {
 	private Rigidbody2D body2d;
 	bool left,rigth,up,down;
-	public static float speed = 1.0f;
+	public static float speed = 0.5f;
 	public static float maxSpeed = 3.0f;
-	public static float Acc=0.1f;
+	public static float Acc=0.05f;
+	public static float friction=50;
 	// Use this for initialization
 	void Awake () {
 		left = rigth = up = down = false;
@@ -21,51 +22,52 @@ public class move : MonoBehaviour {
 	void Update () {
 		Vector2 v = new Vector2(0,0);
 
+		if (!Input.GetKey (KeyCode.W) && !Input.GetKey (KeyCode.S) && !Input.GetKey (KeyCode.A) && !Input.GetKey (KeyCode.D)) {
+			speed = speed - speed / friction;
 
+			if (up)
+				v.y = speed;
+			if (down)
+				v.y = -speed;
+			if (left)
+				v.x = -speed;
+			if (rigth)
+				v.x = speed;
+			
+		} else {
+			if (!Input.GetKey (KeyCode.W))
+				up = false;
+			if (!Input.GetKey (KeyCode.S))
+				down = false;
+			if (!Input.GetKey (KeyCode.A))
+				left = false;
+			if (!Input.GetKey (KeyCode.D))
+				rigth = false;
+		}
 
 		if (Input.GetKey (KeyCode.W)) {
-			if (!up) {
-				up = true;
-				down = rigth = left = false;
-				speed = 1.0f;
-			} else {
-				if (speed < maxSpeed)
-					speed += Acc;
-
-			}
+			up = true;
+			if (speed < maxSpeed)
+				speed += Acc;
 			v.y = speed;
 		}
 		if (Input.GetKey (KeyCode.S)) {
-			if (!down) {
-				down = true;
-				up = rigth = left = false;
-				speed = 1.0f;
-			} else {
-				if (speed < maxSpeed)
-					speed += Acc;
-			}
+			down = true;
+			if (speed < maxSpeed && !up)
+				speed += Acc;
 			v.y = -speed;
 		}
 		if (Input.GetKey (KeyCode.A)) {
-			if (!left) {
-				left = true;
-				up = rigth = down = false;
-				speed = 1.0f;
-			} else {
-				if (speed < maxSpeed)
-					speed += Acc;
-			}
+			left = true;
+			if (speed < maxSpeed && !up && !down)
+				speed += Acc;
 			v.x = -speed;
 		}
 		if (Input.GetKey (KeyCode.D)) {
-			if (!rigth) {
-				rigth = true;
-				up = left = down = false;
-				speed = 1.0f;
-			} else {
-				if (speed < maxSpeed)
-					speed += Acc;
-			}
+			
+			rigth = true;
+			if (speed < maxSpeed && !up && !down && !left)
+				speed += Acc;
 			v.x = speed;
 		}
 		body2d.velocity = v;
