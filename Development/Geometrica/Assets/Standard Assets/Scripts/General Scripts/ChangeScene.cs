d47545563c4fd0253182d2c747c41dd6c0d5c869
Aria <ark.d.night@gmail.com>
2016-05-25 16:36:00 +0430

    Restart Button Added
    Added Restart Button
    Added Script for Restarting
    Added Script for Scene Changing

---
+++ Development/Geometrica/Assets/Standard Assets/Scripts/General Scripts/ChangeScene.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+	public void ChangeSceneTo (int SceneIndex) {
+		Application.LoadLevel (SceneIndex);
+	}
+
+}
