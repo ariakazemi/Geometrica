75e2c00acd10785a6ac1c4efc7aa1c5d1b89c017
sepehr <sepehr2012a@gmail.com>
2016-06-25 13:14:58 +0430

    may the force be with you
    camera and player

--- Development/Geometrica/Assets/Standard Assets/Scripts/General Scripts/pointerMove.cs
+++ Development/Geometrica/Assets/Standard Assets/Scripts/General Scripts/pointerMove.cs
@@ -5,33 +5,41 @@ public class pointerMove : MonoBehaviour {
 	private Rigidbody2D body2d;
 	Vector2 newPosition;
 	private float xVal,yVal,destance;
-	Vector2 v;
+	Vector2 v,lastposition;
+	public float force=200;
 	void Start () {
 		body2d = GetComponent<Rigidbody2D> ();
 		newPosition = transform.position;
 		v=new Vector2(0,0);
+		lastposition = transform.position;
 	}
 	void Update()
 	{
-		if (Input.GetMouseButtonDown(0))
-		{
+		if (Input.GetMouseButtonDown (0)) {
 			RaycastHit hit;
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			if (Physics.Raycast(ray, out hit))
-			{
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			if (Physics.Raycast (ray, out hit)) {
 				newPosition = hit.point;
-				xVal =-( transform.position.x - newPosition.x);
-				yVal =-( transform.position.y - newPosition.y);
-				destance = Mathf.Sqrt(xVal * xVal + yVal * yVal);
-				v.x= xVal / destance;
-				v.y= yVal / destance;
+				xVal = -(transform.position.x - newPosition.x);
+				yVal = -(transform.position.y - newPosition.y);
+				destance = Mathf.Sqrt (xVal * xVal + yVal * yVal);
+				v.x = xVal / destance;
+				v.y = yVal / destance;
 
 			}
+		
+			xVal = transform.position.x - newPosition.x;
+			yVal = transform.position.y - newPosition.y;
+			Vector2 slower;
+			slower.x= transform.position.x - lastposition.x;
+			slower.y= transform.position.y - lastposition.y;
+			body2d.AddRelativeForce (-10*force * slower);
+			body2d.AddForce (force * v);
+			//body2d.AddForce (force*v);
 		}
-		xVal = transform.position.x - newPosition.x;
-		yVal = transform.position.y - newPosition.y;
+		lastposition=transform.position;
 		//transform.position.x += xVal / destance;
 		//transform.position.y += yVal / destance;
-		body2d.velocity = v;
+		//body2d.velocity = v;
 	}
 }
