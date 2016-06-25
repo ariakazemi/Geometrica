4a770648061045b65905a3eae1d27012928da4a0
sepehr <sepehr2012a@gmail.com>
2016-06-25 11:30:36 +0430

    hichi

---
+++ Development/Geometrica/Assets/Standard Assets/Scripts/General Scripts/cameraMove.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraMove : MonoBehaviour {
+	private Transform player;
+	private Rigidbody2D body2d;
+	private float speed=0.01f;
+	private Transform background;
+	//public Camera camera;
+	// Use this for initialization
+	Vector2 v,LastPlayerPos,NextPlayerpos;
+
+	void Start () {
+		body2d = GetComponent<Rigidbody2D> ();
+		player = GameObject.Find ("Player").transform;
+		background = GameObject.Find ("Background").transform;
+		LastPlayerPos = player.position;
+		v=new Vector2(0,0);
+	
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		//player.
+		NextPlayerpos = player.position;
+		//SleepTimeout (0.1);
+		Vector3 camera =transform.position;
+		//playerpos.z = transform.position.z;
+		/*if (NextPlayerpos.x - transform.position.x > 0.5 )
+			v.x = 1;
+		if (NextPlayerpos.x - transform.position.x < -0.5)
+			v.x = -1;
+		if (NextPlayerpos.y - transform.position.y > 0.5 )
+			v.y = 1;
+		if (NextPlayerpos.y - transform.position.y < -0.5)
+			v.y = -1;
+		*/
+
+		if (NextPlayerpos.x - transform.position.x > 0.5 || NextPlayerpos.x - transform.position.x < -0.5 || NextPlayerpos.y - transform.position.y > 0.5 || NextPlayerpos.y - transform.position.y < -0.5) {
+			camera.x += -(LastPlayerPos.x - NextPlayerpos.x);
+			camera.y += -(LastPlayerPos.y - NextPlayerpos.y);
+		}
+		if (background.position.x - camera.x > 4.35 || background.position.x - camera.x < -4.35 ){
+			camera.x=transform.position.x;
+		}
+		/*if (background.position.y - camera.y > 3.02 || background.position.y - camera.y < -3.02 ){
+			camera.y=transform.position.y;
+		}*/
+		//print (Screen.width);
+		transform.position = camera;
+
+		LastPlayerPos = player.position;
+	}
+}
---
+++ Development/Geometrica/Assets/Standard Assets/Scripts/General Scripts/pointerMove.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pointerMove : MonoBehaviour {
+	private Rigidbody2D body2d;
+	Vector2 newPosition;
+	private float xVal,yVal,destance;
+	Vector2 v;
+	void Start () {
+		body2d = GetComponent<Rigidbody2D> ();
+		newPosition = transform.position;
+		v=new Vector2(0,0);
+	}
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast(ray, out hit))
+			{
+				newPosition = hit.point;
+				xVal =-( transform.position.x - newPosition.x);
+				yVal =-( transform.position.y - newPosition.y);
+				destance = Mathf.Sqrt(xVal * xVal + yVal * yVal);
+				v.x= xVal / destance;
+				v.y= yVal / destance;
+
+			}
+		}
+		xVal = transform.position.x - newPosition.x;
+		yVal = transform.position.y - newPosition.y;
+		//transform.position.x += xVal / destance;
+		//transform.position.y += yVal / destance;
+		body2d.velocity = v;
+	}
+}
