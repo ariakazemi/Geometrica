138b7378d6a9b80cd3fe4a471a39ed5bf5d89318
Aria <ark.d.night@gmail.com>
2016-06-28 21:09:02 +0430

    Add: Tutorial Scene First Components

---
+++ Development/Geometrica/Assets/Standard Assets/Scripts/Tutorial/Cutscenes/Begining.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Begining : MonoBehaviour {
+	Animator anim;
+
+	void Awake() {
+		GameObject window = GameObject.Find ("Window");
+		anim = window.GetComponent<Animator> ();
+
+	}
+	public void CloseWindow() {
+		anim.SetTrigger("Close");
+	}
+}
